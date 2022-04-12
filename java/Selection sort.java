ddd263f162c2f5efeeee4c21dd863d45300d6fdc
KimSangWoo <swkiim1229@gmail.com>
2022-04-12 14:36:24 +0900

    Create Selection sort.java
    선택정렬 알고리즘

---
+++ java/Selection sort.java
@@ -0,0 +1,30 @@
+public class Test
+{
+	public static void main(String[] args)
+    {
+    	int a[] = {10, 53, 35, 100, 87}
+        int i = 0;
+        int temp = 0;
+        do
+        {
+        	int j = i;
+            do
+            {
+            	if(a[i] > a[j])
+                {
+                	temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+                j++;
+            }
+            while(j < 5);
+            i++;
+        }
+        while(i < 4);
+        for (int b = 0; b < 5; b++)
+        {
+        	System.out.printf(a[b] + "\t");
+        }
+    }
+}
